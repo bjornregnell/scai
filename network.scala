91885be44cbe3627e6aa32116a08eecda6c849b4
bjornregnell <bjorn.regnell@cs.lth.se>
2023-03-18 21:26:40 +0100

    improve comments and readability

--- network.scala
+++ network.scala
@@ -1,26 +1,22 @@
+/** A Layer consists of many Neurons */
 type Layer = Array[Neuron]
 
 /** A simple model of a brain with several neurons in several layers. */
 class Network(val inputSize: Int, val layerSizes: List[Int]):
-  val input = new Vec(inputSize)
+  val input   = new Vec(inputSize)
   val outputs = new Array[Vec](layerSizes.length)
   val neurons = new Array[Layer](layerSizes.length)
-
-  def randomNeuron(): (Int, Int) = 
-    val layer = util.Random.nextInt(layerSizes.length)
-    val index = util.Random.nextInt(layerSizes(layer))
-    (layer, index)
-
   val lastLayer = layerSizes.length - 1
   
-  // allocate neurons in layers
   for layer <- 0 until layerSizes.length do 
-    outputs(layer) = new Vec(layerSizes(layer))
+    // make room for neurons in layers and output vectors between layers
     neurons(layer) = new Layer(layerSizes(layer))
+    outputs(layer) = new Vec(layerSizes(layer))
     for index <- 0 until layerSizes(layer) do 
-      if layer == 0 then 
+      if layer == 0 
+      then // neurons in the first layer are connected to input
         neurons(layer)(index) = new Neuron(input)
-      else
+      else // other neurons are connected to the output of the previous layer
         neurons(layer)(index) = new Neuron(outputs(layer - 1))
   
   /** Walk through all neurons in all layers and forward outputs to next layer */
@@ -29,32 +25,26 @@ class Network(val inputSize: Int, val layerSizes: List[Int]):
       for index <- outputs(layer).indices do
         outputs(layer)(index) = neurons(layer)(index).output()
 
-  /** Use signal as input and feeding forward to output of last layer. */
+  /** Use signal as input and feed forward to subsequent layers. */
   def predict(signal: Vec): Vec =
     for i <- input.indices do input(i) = signal(i)
     feedForward()
     outputs(lastLayer)
   
-  /** Show this network with its neurons in each layer */
-  def show: String = 
-    val heading = s"Neural Network [inputSize=$inputSize, layerSizes=$layerSizes]"
-    var body = 
-      (
-        for layer <- 0 until layerSizes.length yield
-          s"Layer $layer: " ++ (
-            for index <- 0 until layerSizes(layer) 
-            yield neurons(layer)(index).show
-          ).mkString(", ")
-      ).mkString("\n")
-    s"$heading\n$body"
-
+  /** Return layer and index of a randomly picked neuron in this network */
+  def randomNeuron(): (Int, Int) = 
+    val layer = util.Random.nextInt(layerSizes.length)
+    val index = util.Random.nextInt(layerSizes(layer))
+    (layer, index)
+  
+  /** Pick a random neuron and mutate its parameters.*/
   def mutateRandomNeuron(learningFactor: Num): (Int, Int) = 
     val (l, i) = randomNeuron()
     neurons(l)(i).mutate(learningFactor)
     (l, i)
   
+  /** Run training cycles using data. The learningFactor controls the size of mutations.*/
   def train(cycles: Int, data: DataSet, learningFactor: Num = 0.3): Unit = 
-
     def computeLoss(): Num = 
       var averageLoss = 0.0
       var i = 0
@@ -75,5 +65,17 @@ class Network(val inputSize: Int, val layerSizes: List[Int]):
       if loss2 < loss1
       then neurons(l)(i).save() 
       else neurons(l)(i).backtrack()
-
   end train
+  
+  /** Show this network with its neurons in each layer */
+  def show: String = 
+    val heading = s"Neural Network [inputSize=$inputSize, layerSizes=$layerSizes]"
+    var body = 
+      (
+        for layer <- 0 until layerSizes.length yield
+          s"Layer $layer: " ++ (
+            for index <- 0 until layerSizes(layer) 
+            yield neurons(layer)(index).show
+          ).mkString(", ")
+      ).mkString("\n")
+    s"$heading\n$body"
--- neuron.scala
+++ neuron.scala
@@ -1,47 +1,41 @@
 class Neuron(val input: Vec):
-  def show: String = s"Neuron[input.size=${input.size}]"
-
   var bias: Num = random()
-
   var weights: Vec = Array.fill(input.size)(random())
 
   /** Initiate this neuron to a random state. */
   def reset(): Unit =
     bias = random()
-    var i = 0
-    while i < weights.length do 
+    for i <- weights.indices do 
       weights(i) = random()
-      i += 1
   
   /** Randomly adjust the state, scaled by factor. */
   def mutate(factor: Num): Unit = 
-    bias +=  factor * random()
-    var i = 0
-    while i < input.size do 
-      weights(i) += factor * random()
-      i += 1
+    bias = bias + factor * random()
+    for i <- weights.indices do 
+      weights(i) = weights(i) + factor * random()
 
   /** Compute output value. The sigmoid call constrains output in [0..1] */ 
-  def output(): Num = sigmoid(multiply(weights, input) + bias)
+  def output(): Num = 
+    val x = multiply(weights, input) + bias 
+    sigmoid(x)
 
-  /** Memory to save the current bias*/
+  /** Memory for saving the current bias*/
   var savedBias: Num = bias
 
-  /** Memory to save the current weights*/
+  /** Memory for saving the current weights*/
   var savedWeights: Vec = weights.clone()
   
   /** Forget current state and restore saved state */
   def backtrack(): Unit = 
     bias = savedBias
-    var i = 0
-    while i < input.size do 
+    for i <- weights.indices do 
       weights(i) = savedWeights(i)
-      i += 1
   
   /** Remember current state. */
   def save(): Unit =
     savedBias = bias
-    var i = 0
-    while i < input.size do 
+    for i <- weights.indices do 
       savedWeights(i) = weights(i)
-      i += 1
+  
+  def show: String = s"Neuron[input.size=${input.size}]"
+end Neuron
