0825e489136d6a00cb35cd31e43327e0a57da4f5
bjornregnell <bjorn.regnell@cs.lth.se>
2023-03-18 21:51:07 +0100

    improve comments and readability

--- main-program.scala
+++ main-program.scala
@@ -2,18 +2,22 @@
 
 val welcomeMessage = "Welcome to AI SEX CLASSIFIER"
 
-val trainData = DataSet.fromFile("train-data.txt")
-val testData  = DataSet.fromFile("test-data.txt")
-
+/** Convert a number between 0 and 1 to a binary sex. */
 def binaryClassifier(x: Num): String = 
   if x > 0.5 
   then "Female" 
   else "Male  "
 
+val trainData = DataSet.fromFile("train-data.txt")
+val testData  = DataSet.fromFile("test-data.txt")
+
 val ai = new Network(inputSize = trainData.inputs(0).size, layerSizes = List(3,2,1))
 
-def showColor(s: String, color: String): String = color + s + Console.RESET
+/** Show any text in color in terminal using e.g. the color Console.RED */
+def showColor(text: String, colorCode: String): String = 
+  colorCode + text + Console.RESET
 
+/** Use data to test our ai. A loss close to zero represents high certainty. **/
 def test(data: DataSet): Unit =
   for i <- data.inputs.indices do
     val predicted = ai.predict(data.inputs(i))
@@ -30,6 +34,7 @@ def test(data: DataSet): Unit =
       s"correct=${binaryClassifier(correct(0))} ${correct.mkString(",")}  " +
       s"predicted=$showPredicted  ${predicted.mkString(",")} loss=$loss") 
 
+/** The main program. Run it in terminal with `scala-cli run .` */
 @main def run = 
   println(s"\n--- $welcomeMessage\n")
   println(ai.show)
